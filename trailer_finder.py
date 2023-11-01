5db7a4429a7c353c962aee0cd91ba87cd70258a7
Aayush <aayushsoni424@gmail.com>
2023-11-01 16:53:06 +0530

    python script of movie trailer finder

---
+++ trailer_finder.py
@@ -0,0 +1,23 @@
+import requests
+import webbrowser
+from bs4 import BeautifulSoup
+
+def findYTtrailer(query):
+    query = query.split(" ")
+    query = '+'.join(query)
+    search_url = f"https://www.youtube.com/results?search_query={query}"
+
+    # Send an HTTP GET request to YouTube
+    response = requests.get(search_url)
+
+    if response.status_code == 200:
+        # Parse the HTML response
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        # Convert the soup into a string
+        soup = str(soup)
+        
+        # Find the index where 'href="/watch?v=' is present in the soup_str
+        index = soup.find('watch?v=')
+        video_url = "https://www.youtube.com"+soup[index-1:soup.find('\\',index+10)]
+        return video_url
