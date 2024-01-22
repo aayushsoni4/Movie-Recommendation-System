a4cdc4ec1f3106f82f1877642f1e55a81e21b54d
Aayush <aayushsoni424@gmail.com>
2024-01-22 15:29:28 +0530

    feteching trailer usign API

--- trailer_finder.py
+++ trailer_finder.py
@@ -1,22 +1,26 @@
-import requests
-from bs4 import BeautifulSoup
+from googleapiclient.discovery import build
+from dotenv import load_dotenv 
+import os
 
-def findYTtrailer(query):
-    query = query.split(" ")
-    query = '+'.join(query)
-    search_url = f"https://www.youtube.com/results?search_query={query}"
+load_dotenv()
+YT_API = os.environ['YT_API']
+youtube = build('youtube', 'v3', developerKey=YT_API)
 
-    # Send an HTTP GET request to YouTube
-    response = requests.get(search_url)
+def findYTtrailer(movie_title):
+    search_query = movie_title
 
-    if response.status_code == 200:
-        # Parse the HTML response
-        soup = BeautifulSoup(response.text, 'html.parser')
-        
-        # Convert the soup into a string
-        soup = str(soup)
-        
-        # Find the index where 'href="/watch?v=' is present in the soup_str
-        index = soup.find('watch?v=')
-        video_url = "https://www.youtube.com"+soup[index-1:soup.find('\\',index+10)]
-        return video_url
+    request = youtube.search().list(
+        part="snippet",
+        q=search_query,
+        type="video",
+        order="relevance",
+        videoCategoryId="1"
+    )
+
+    response = request.execute()
+
+    item = response['items'][0]
+    video_id = item['id']['videoId']
+    video_url = f"https://www.youtube.com/watch?v={video_id}"
+
+    return video_url
